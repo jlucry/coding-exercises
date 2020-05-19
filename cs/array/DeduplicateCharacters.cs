bc68592ffdaf33742a51bd42d7b39b6349297bd4
Jean-Olivier LUCRY <jlucry@yahoo.com>
2020-05-19 23:01:32 +0200

    add csharp array section

---
+++ cs/array/DeduplicateCharacters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace array
+{
+    public static class DeduplicateCharacters
+    {
+        public static string RunBasic(string source)
+        {
+            int[] ascii = new int[256];
+            
+            for(int i=0; i<256; i++)
+            {
+                ascii[i] = 0;
+            } 
+
+            int resultLenght = 0;
+
+            for(int i=0; i<source.Length ;i++)
+            {
+                int charCode = source[i];
+                ascii[charCode]++;
+
+                if(ascii[charCode]==1)
+                {
+                    resultLenght++;
+                }
+            }
+
+            char[] result = new char[resultLenght];
+
+            int j = 0;
+            for(int i=0; i<source.Length ;i++)
+            {
+                int charCode = source[i];
+
+                if(ascii[charCode]>=1)
+                {
+                    result[j] = source[i];
+                    ascii[charCode]=0;
+                    j++;
+                }
+            }
+
+            return new string(result);
+        }
+    }
+}
---
+++ cs/array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Hello, I want you to remove some letters. Thank!";
+            string toRemove = "aeiouy";
+            
+            Console.WriteLine(sentence);
+            Console.WriteLine(toRemove);
+
+            string result = RemoveCharacters.RunEnhanced(sentence, toRemove);
+            Console.WriteLine(result+":"+result.Length);
+            result = RemoveCharacters.RunBasic(sentence, toRemove);
+            Console.WriteLine(result+":"+result.Length);
+
+            sentence = "Hello google go fast";
+            Console.WriteLine(sentence);
+            result = DeduplicateCharacters.RunBasic(sentence);
+            Console.WriteLine(result+":"+result.Length);
+        }
+    }
+}
